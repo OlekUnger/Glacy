e73703d8440c60798aec024c829a6251116453eb
Oleg Unger <ounger@inbox.ru>
2016-07-21 17:38:51 +0300

    -

--- js/common.js
+++ js/common.js
@@ -13,3 +13,39 @@ search_btn.addEventListener("mouseover", function(event) {
 search_field.addEventListener("mouseout", function(event) {
   search_form.classList.remove("modal-content-show");
 });
+
+$(function() {
+                /*************  Initialize Price Range Slider *************/
+                $( "#slider-range" ).slider({
+                        range: true,
+                        min: 0,
+                        max: 900,
+                        step: 10, // Use this determine the amount of each interval
+                        values: [ 100, 500 ], // The default range
+                        slide: function( event, ui ) {
+                                $( "#min-price" ).val( ui.values[ 0 ]);// for input text box
+                                $( "#max-price" ).val(ui.values[ 1 ]);
+                                $( "#my_min" ).val(ui.values[ 0 ]); // Display and selected the min Price
+                                $( "#my_max" ).val(ui.values[ 1 ]); // Display and selected the max Price
+                        }
+                });
+        /*************  Set Initial Value of the Dropdown Box *************/
+                //For dropdown box
+                $( "#my_min" ).val($( "#slider-range" ).slider( "values", 0 ));
+                $( "#my_max" ).val($( "#slider-range" ).slider( "values", 1 ));
+        
+
+   /************* When user choose from dropdown box directly *************/
+                $("select.price_range").change(function () {
+                        $myMinValue = $("#my_min option:selected").val();
+                        $myMaxValue = $("#my_max option:selected").val();
+                        //Make changes on the slider itself
+                        if($myMinValue <= $myMaxValue) {
+                                $( "#slider-range" ).slider({
+                                        values: [$myMinValue, $myMaxValue]                                                     
+                                });
+                        } else {
+                                alert("Invalid Input");
+                        }
+                });
+          });
